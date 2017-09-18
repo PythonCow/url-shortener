d2b96f7080cda9dc6b9e7a539404fa4fc6dfcb98
Ben Jamison <ben@debian.Ben>
2017-09-18 15:53:59 -0600

    Initial commit.

---
+++ server.js
@@ -0,0 +1,46 @@
+var app = require("express")();
+var mongo = require("mongodb").MongoClient;
+
+
+app.get("/:id", function(request, response){
+  //Find id in database and send appropriate url.
+  mongo.connect("mongodb://127.0.0.1/database", function(err, db){
+    if(err) throw err;
+    var urls = db.collection("urls");
+    urls.find({_id: parseInt(request.params.id)}).toArray(function(err, results){
+      if(results[0]!== undefined){
+        response.send("<!DOCTYPE html>\n<script>window.location='https://"+results[0].url+"'</script>");
+      } else {
+        response.send("<!DOCTYPE html>\n<h1>404 Not Found.</h1>")
+      }
+    });
+  })
+});
+
+app.get("/new/:url", function(request, response){
+  //Insert url into database and send id and url back.
+  mongo.connect("mongodb://localhost:27017/database", function(err, db){
+    if(err) throw err;
+    var urls = db.collection("urls");
+    urls.aggregate([
+      {$group: {
+        _id: "Largest_Id",
+        largest_id: {$max: "$_id"}
+      }}
+    ]).toArray(function(err, results){
+      if(err) throw err;
+      var id = results[0].largest_id+1;
+      urls.insert({
+        _id: id,
+        url: request.params.url
+      }, function(err){
+        if(err) throw err;
+      });
+      response.send({
+        "shortened-url": "https://"+request.headers.host+"/"+id
+      });
+    });
+  });
+});
+
+app.listen(8000);
